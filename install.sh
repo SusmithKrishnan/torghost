5c27bc50445a28e4b0c55dc18036693015989514
susmithHCK <tesla8.1997@gmail.com>
2016-10-15 11:20:18 +0530

    Installer

---
+++ install.sh
@@ -0,0 +1,10 @@
+clear
+echo "******* Torghost installer ********"
+echo ""
+echo "=====> Installing tor bundle "
+sudo apt-get install tor -y -qq
+echo "=====> Installing TorGhost "
+sudo cp torghost /usr/bin/torghost
+sudo chmod +x /usr/bin/torghost
+echo "=====> Done "
+echo "=====> Open terminal and type 'torghost' for usage "
