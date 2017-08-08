6197412c2707d1c0b76821095ba39521f4f39bde
susmithHCK <tesla8.1997@gmail.com>
2017-08-08 16:54:33 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ echo "******* Torghost installer ********"
 echo ""
 echo "=====> Installing tor bundle "
 sudo apt-get install tor -y -qq
+echo "=====> Installing dependencies "
+sudo pip install stem
 echo "=====> Installing TorGhost "
 sudo cp torghost /usr/bin/torghost
 sudo chmod +x /usr/bin/torghost
