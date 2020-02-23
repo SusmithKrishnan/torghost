923fdc07032089920b839ef29006b457435c2e5f
gmkbenjamin <gmkbenjamin@hotmail.com>
2020-02-23 02:22:15 +0000

    build app into one single executable to avoid missing library errors.

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@ echo "Installing prerequisites "
 sudo apt-get install tor python-pip -y 
 echo "Installing dependencies "
 pip install -r requirements.txt 
-pyinstaller torghost.py
-cp -r dist/torghost/ /usr/share/
-ln -sf /usr/share/torghost/torghost /usr/bin/torghost
+pyinstaller --onefile torghost.py
+cp -r dist/torghost /usr/bin/
