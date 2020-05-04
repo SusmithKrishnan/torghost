31933c7134524e9bb002ca67f6e10caa6be49f9e
Nautilus567 <Nautilus567@users.noreply.github.com>
2020-05-03 22:17:37 -0400

    Python 3 code update

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 echo "Torghost installer v3.0"
 echo "Installing prerequisites "
-sudo apt-get install tor python-pip -y 
+sudo apt-get install tor python3-pip -y 
 echo "Installing dependencies "
-sudo pip2 install -r requirements.txt 
+sudo pip3 install -r requirements.txt 
 pyinstaller --onefile torghost.py
 sudo cp -r dist/torghost /usr/bin/
